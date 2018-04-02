7ebd984ffd603717bb758e55fddd70ac6808b666
HiroZhou <xiong.zhou@kankanapp.com.cn>
2018-04-02 21:40:36 +0800

    remove ad

--- app/Http/Controllers/DapentiController.php
+++ app/Http/Controllers/DapentiController.php
@@ -1,15 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use \Curl\Curl;
 use Sunra\PhpSimple\HtmlDomParser;
 use Illuminate\Http\Request;
 
 class DapentiController extends Controller
 {
-
-
-    private function get($url){
+    private function get($url)
+    {
         $curl = new Curl();
         $curl->setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36');
         $curl->setReferrer('http://www.dapenti.com');
@@ -25,11 +25,13 @@ class DapentiController extends Controller
         }
     }
 
-    private function replaceImage($url){
+    private function replaceImage($url)
+    {
         return str_replace('http://pic.yupoo.com/dapenti/', route('dapenti.image').'?url=', $url);
     }
 
-    public function image(Request $request){
+    public function image(Request $request)
+    {
         $url = 'http://pic.yupoo.com/dapenti/'.$request->get('url');
         $curl = new Curl();
         $curl->setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36');
@@ -46,11 +48,11 @@ class DapentiController extends Controller
     {
         $url = 'http://www.dapenti.com/blog/index.asp';
         $response = $this->get($url);
-        $html = HtmlDomParser::str_get_html( iconv('GBK',"UTF-8//IGNORE", $response) );
+        $html = HtmlDomParser::str_get_html(iconv('GBK', "UTF-8//IGNORE", $response));
         $content = [];
-        foreach($html->find('div.center_title_down',0)->find('li') as $i){
+        foreach ($html->find('div.center_title_down', 0)->find('li') as $i) {
             $news['title'] = $i->plaintext;
-            $news['link'] = route('dapenti.show', ['id' => explode('id=', $i->find('a',0)->href)[1]]);
+            $news['link'] = route('dapenti.show', ['id' => explode('id=', $i->find('a', 0)->href)[1]]);
             $content[] = $news;
         }
         return view('dapenti.site_index', ['title' => 'index', 'content' => $content]);
@@ -76,22 +78,19 @@ class DapentiController extends Controller
         $content = $result->filter()->values()->all();
         // dd($content);
         return view('dapenti.list', ['title' => $type, 'content' => $content]);
-
     }
 
     public function show($id)
     {
         $url = 'http://www.dapenti.com/blog/more.asp?name=xilei&id='.$id;
         $response = $this->get($url);
-        $plainthtml = HtmlDomParser::str_get_html( iconv('GBK',"UTF-8//IGNORE", $response));
-        $title = explode('--', $plainthtml->find('title',0)->innertext())[1];
-        $html = $plainthtml->find('div.oblog_text',0);
-        foreach ($html->find('script') as $ad)
-        {
+        $plainthtml = HtmlDomParser::str_get_html(iconv('GBK', "UTF-8//IGNORE", $response));
+        $title = explode('--', $plainthtml->find('title', 0)->innertext())[1];
+        $html = $plainthtml->find('div.oblog_text', 0);
+        foreach ($html->find('script') as $ad) {
             $ad->outertext = '';
         }
-        foreach ($html->find('ins') as $ad)
-        {
+        foreach ($html->find('ins') as $ad) {
             $ad->outertext = '';
         }
         $strs = [
@@ -101,12 +100,12 @@ class DapentiController extends Controller
             '每天一图卦，让我们',
             '海外访问，请加',
             '友情提示：请各位河蟹评论',
-            '喷嚏新浪围脖'
+            '喷嚏新浪围脖',
+            '我想问你这裤子哪里买的'
         ];
-        foreach ($html->find('p') as $p)
-        {
+        foreach ($html->find('p') as $p) {
             foreach ($strs as $str) {
-                if(str_contains($p->innertext, $str)){
+                if (str_contains($p->innertext, $str)) {
                     $p->outertext = '';
                 }
             }
@@ -114,10 +113,9 @@ class DapentiController extends Controller
                 $p->outertext = '';
             }
         }
-        $content = str_replace('<hr>广告<br><!-- bigbanner --><br><hr>', '',$html);
-        $content = str_replace('<b>免责申明：</b>', '',$content);
+        $content = str_replace('<hr>广告<br><!-- bigbanner --><br><hr>', '', $html);
+        $content = str_replace('<b>免责申明：</b>', '', $content);
         $content = $this->replaceImage($content);
         return view('dapenti.show', ['title' => $title, 'content' => $content]);
     }
-
 }
