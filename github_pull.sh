afe3b70e171611dcece485e422190a886b3fd573
HiroZhou <xiong.zhou@poper.jp>
2018-05-09 21:39:54 +0900

    update pwd

--- github_pull.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#export LANG=zh_CN.UTF-8
-echo pwd
-git fetch --all && git reset --hard origin/master
-echo "生产环境更新完毕"
--- public/github_pull.sh
+++ public/github_pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #export LANG=zh_CN.UTF-8
-echo pwd
+echo $PWD
 git fetch --all && git reset --hard origin/master
 echo "生产环境更新完毕"
